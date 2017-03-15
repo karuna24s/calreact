3c2fe7a120e8d91ac2887e37d98ad996594f88ad
karuna24s <karuna24s@gmail.com>
2017-03-15 12:43:03 -0400

    updated routes.rb and add resources :appointments which resolved the no method error

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -2,17 +2,6 @@ class AppointmentsController < ApplicationController
   def index
     @appointments = Appointment.order('appt_time ASC')
     @appointment = Appointment.new
-    # @appointments = Appointment.order('appt_time ASC')
-    # @appointment = Appointment.new
   end
-
-  # def create
-  #   @appointment = Appointment.create(appointment_params)
-  #   @appointments = Appointment.order('appt_time ASC')
-  # end
-  #
-  # private
-  # def appointment_params
-  #   params.require(:appointment).permit(:title, :appt_time)
-  # end
+  
 end
