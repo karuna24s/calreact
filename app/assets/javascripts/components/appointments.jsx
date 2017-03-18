e1983f8d21a4b6d309c8e332c128e75e51de5324
karuna24s <karuna24s@gmail.com>
2017-03-18 19:09:41 -0400

    Add initial state values

--- app/assets/javascripts/components/appointments.jsx
+++ app/assets/javascripts/components/appointments.jsx
@@ -1,10 +1,20 @@
 var Appointments = React.createClass({
+
+  getInitialState: function() {
+    return {
+      appointments: this.props.appointments,
+      input_title: 'Standup Meeting',
+      input_appt_time: 'Monday 10am'
+    }
+  },
+
   render: function() {
     return (
       <div>
-        <AppointmentForm />
-        <AppointmentsList appointments={this.props.appointments} />
+        <AppointmentForm input_title={this.state.input_title} input_appt_time={this.state.input_appt_time} />
+        <AppointmentsList appointments={this.state.appointments} />
       </div>
     )
   }
+  
 });
