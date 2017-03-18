b8fe8fadd7fbea79849827ef100400fda87ab406
karuna24s <karuna24s@gmail.com>
2017-03-18 19:36:05 -0400

    Handle user input into form text fields (being able to edit input) and set state

--- app/assets/javascripts/components/appointments.jsx
+++ app/assets/javascripts/components/appointments.jsx
@@ -3,18 +3,24 @@ var Appointments = React.createClass({
   getInitialState: function() {
     return {
       appointments: this.props.appointments,
-      input_title: 'Standup Meeting',
-      input_appt_time: 'Monday 10am'
+      title: 'Standup Meeting',
+      appt_time: 'Monday 10am'
     }
+  },
+
+  handleUserInput: function(obj) {
+    this.setState(obj);
   },
 
   render: function() {
     return (
       <div>
-        <AppointmentForm input_title={this.state.input_title} input_appt_time={this.state.input_appt_time} />
+        <AppointmentForm input_title={this.state.title}
+         input_appt_time={this.state.appt_time}
+         onUserInput={this.handleUserInput} />
         <AppointmentsList appointments={this.state.appointments} />
       </div>
     )
   }
-  
+
 });
