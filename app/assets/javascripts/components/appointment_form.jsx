9aa2e7db2aa0c59b35fddef87eecdb237e940bfe
karuna24s <karuna24s@gmail.com>
2017-03-19 15:05:50 -0400

    Add datepicker calendar widget using react-datetime component library

--- app/assets/javascripts/components/appointment_form.jsx
+++ app/assets/javascripts/components/appointment_form.jsx
@@ -1,10 +1,8 @@
 var AppointmentForm = React.createClass({
-
   handleChange: function(e) {
     var name = e.target.name;
     obj = {};
     obj[name] = e.target.value;
-    console.log(obj);
     this.props.onUserInput(obj);
   },
 
@@ -13,17 +11,31 @@ var AppointmentForm = React.createClass({
     this.props.onFormSubmit();
   },
 
+  setApptTime: function(e) {
+    var name = 'appt_time';
+    var obj = {};
+    if(obj[name] = e.toDate()) {
+      this.props.onUserInput(obj);
+    }
+  },
+
   render: function() {
+    var inputProps = {
+      name: 'appt_time'
+    };
+
     return (
       <div>
         <h2>Make a new appointment</h2>
         <form onSubmit={this.handleSubmit}>
           <input name='title' placeholder='Appointment Title'
-           value={this.props.title}
-           onChange={this.handleChange} />
-          <input name='appt_time' placeholder='Date and Time'
-           value={this.props.appt_time}
-           onChange={this.handleChange} />
+            value={this.props.input_title}
+            onChange={this.handleChange} />
+
+          <Datetime input={false} open={true} inputProps={inputProps}
+            value={this.props.appt_time}
+            onChange={this.setApptTime} />
+
           <input type='submit' value='Make Appointment' />
         </form>
       </div>
