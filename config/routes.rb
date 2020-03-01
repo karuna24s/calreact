ea1653097ef34854d449e35200cbd8932bb57b33
karuna24s <karuna24s@gmail.com>
2020-03-01 01:09:55 -0500

    update routes and remove unnecessary files

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-
-  root 'appointments#index'
-
-  resources :appointments
+  # 
+  # root 'appointments#index'
+  #
+  # resources :appointments
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
